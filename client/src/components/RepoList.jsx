a232eab4809d461449b8f02d8d21c4a74b958bfa
Zack Miller <hotbodyfitness@gmail.com>
2019-07-24 15:47:13 -0700

    Complete repo name linking to github

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-const RepoList = (props) => (
-  <div>
-    <h4><u> Newest {props.repos.length ? props.repos.length : <p hidden></p>} Repos </u></h4>
-    <ol>
-      {props.repos.map((e, i) => {
-        return (<li key={i}>{e.html_url}</li>)
-      })}
-    </ol>
-    There are now {props.count} total repos saved in the database.
+const RepoList = (props) => {
+  var cap = {'textTransform': 'capitalize'};
+  return (
+    <div>
+      <h4><u> Newest {props.repos.length ? props.repos.length : <p hidden></p>} Repos </u></h4>
+      <ol>
+        {props.repos.map((e, i) => {
+          return (<li key={i}><a href={e.html_url} style={cap}>{e.name}</a><br /> — <b>created by: </b>{e.owner}<br /> — <b>updated at:</b> {e.updated_at}<br /><br /></li>)
+        })}
+      </ol>
+      There are now {props.count} total repos saved in the database.
   </div>
-)
+  )
+};
 
 export default RepoList;
