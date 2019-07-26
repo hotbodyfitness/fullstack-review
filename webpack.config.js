746b2aca58e59b6287215ee3325a7ea5d0f63705
Zack Miller <hotbodyfitness@gmail.com>
2019-07-25 20:47:43 -0700

    Fixed bundle.js location for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,14 @@ module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: __dirname
   },
   module : {
     loaders : [
       {
         test : /\.jsx?/,
         include : SRC_DIR,
-        loader : 'babel-loader',      
+        loader : 'babel-loader',
         query: {
           presets: ['react', 'es2015']
         }
